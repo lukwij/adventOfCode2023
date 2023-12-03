f50cf20cf152f9f88b7b0fccd15880fbf0b6a849
lukwij <lukwij@gmail.com>
2023-12-03 16:43:38 +0100

    day2 complete

--- day02/day02_1.py
+++ day02/day02_1.py
@@ -3,12 +3,37 @@
 import re
 
 
-def get_games(filename: str) -> list:
+def get_games(filename: str) -> dict:
     with open(filename, mode='r') as f:
+        games = {}
         for line in f:
-            result = re.search(r'Game (\d)+: (.*)$', line)
-            pass
+            result = re.search(r'Game (\d+): (.*)$', line)
+
+            draws = []
+            for draw in result.group(2).split(';'):
+                pick = {}
+                for finding in re.findall(r'(\d+) ([a-z]+)', draw):
+                    pass
+                    pick[finding[1]] = int(finding[0])
+                draws.append(pick)
+            games[int(result.group(1))] = draws
+        return games
+
+
+def check_games(condition: dict, input_data: dict) -> int:
+    result = 0
+    for game_number in input_data.keys():
+        game_possible = True
+        for pick in input_data[game_number]:
+            for color in pick.keys():
+                if pick[color] > condition[color]:
+                    game_possible = False
+                    break
+        if game_possible:
+            result += game_number
+    return result
 
 
 if __name__ == "__main__":
-    print(get_games('sample'))
+    game_data = get_games('input')
+    print(check_games({'red': 12, 'green': 13, 'blue': 14}, game_data))
---
+++ day02/day02_2.py
@@ -0,0 +1,23 @@
+# day02_2.py
+
+from day02_1 import get_games
+from math import prod
+
+
+def check_games(input_data: dict) -> int:
+    result = 0
+    for game_number in input_data.keys():
+        min_game_cubes = {'red': 0, 'green': 0, 'blue': 0}
+        for pick in input_data[game_number]:
+            for color in pick.keys():
+                if pick[color] > min_game_cubes[color]:
+                    min_game_cubes[color] = pick[color]
+        game_score = prod(min_game_cubes.values())
+        result += game_score
+        min_game_cubes = {'red': 0, 'green': 0, 'blue': 0}
+    return result
+
+
+if __name__ == "__main__":
+    game_data = get_games('input')
+    print(check_games(game_data))
